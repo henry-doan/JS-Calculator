99320bf584d10997a5b83bfba90e3a44f91ea9e7
Henry Doan <henrydoan96@gmail.com>
2016-06-14 09:09:39 -0600

    Added MOAR Operations

--- app.js
+++ app.js
@@ -37,3 +37,43 @@ function secFun(){
 function cotFun(){
 	result.innerHTML = 1 / Math.tan(result.innerHTML);
 }
+
+function absoluteV(){
+	result.innerHTML = Math.abs(result.innerHTML);
+}
+
+function arcSine(){
+	result.innerHTML = Math.asin(result.innerHTML);
+}
+
+function arcCosine(){
+	result.innerHTML = Math.acos(result.innerHTML);
+}
+
+function arcTangent(){
+	result.innerHTML = Math.atan(result.innerHTML);
+}
+
+function squareRootTwo(){
+	result.innerHTML = Math.SQRT2;
+}
+
+function ranNum(){
+	result.innerHTML = (Math.random()* 11);
+}
+
+function anatLogTwo(){
+	result.innerHTML = Math.LN2(result.innerHTML);
+}
+
+function natLogTen(){
+	result.innerHTML = Math.LN10(result.innerHTML);
+}
+
+function roundUp(){
+	result.innerHTML = Math.ceil(result.innerHTML);
+}
+
+function roundDown(){
+	result.innerHTML = Math.floor(result.innerHTML);
+}
